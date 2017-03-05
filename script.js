4d39f282bd82ba2900d28434c2089ae1b99717ca
prbsas <paularamirez0316@gmail.com>
2017-03-05 18:59:02 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+function displayTime() {
+  var currentTime = new Date();
+  var hours = currentTime.getHours();
+  var minutes = currentTime.getMinutes();
+  var seconds = currentTime.getSeconds();
+
+  var meridiem = 'pm';
+  if(hours > 12) {
+    hours = hours - 12;
+    meridiem = 'am';
+  }
+  if(hours === 0) {
+    hours = 12;
+  }
+
+  if(hours < 10) {
+    hours = '0' + hours;
+  }
+  if(minutes < 10) {
+    minutes = '0' + minutes;
+  }
+  if(seconds < 10) {
+    seconds = '0' + seconds;
+  }
+
+  var clockDiv = document.getElementById('clock');
+  clockDiv.innerText = `${hours}:${minutes}:${seconds}·${meridiem}`;
+}
+displayTime();
+setInterval(displayTime, 1000);
